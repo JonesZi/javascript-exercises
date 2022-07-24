d3ef70f180a0548b64f7309ba31b897795ccadb5
Jonas Zi <jonas.zinnoecker@gmail.com>
2022-07-24 21:58:12 +0200

    Update Solution to 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,14 @@
 const reverseString = function(string) {
-const array = [];
-for (let i = 0; i < string.length; i++) {
-array[i] = string[i];
-}
-const reverseArray = array.reverse();
-const reverseString = reverseArray.join("");
-return reverseString;
+// My solution
+    // const array = [];
+    // for (let i = 0; i < string.length; i++) {
+    // array[i] = string[i];
+    // }
+    // const reverseArray = array.reverse();
+    // const reverseString = reverseArray.join("");
+    // return reverseString;
+//Better solution
+return string.split("").reverse().join("");
 }
 
 // Do not edit below this line
