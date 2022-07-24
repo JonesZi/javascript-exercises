5aca95e259bfbbdd3981b68471c23de6e63f7373
Jonas Zi <jonas.zinnoecker@gmail.com>
2022-07-25 00:10:21 +0200

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+       const filteredArray = array.filter(element => args.indexOf(element) === -1
+       
+    // Longer way (without arrow / one line)
+    // function(element) {
+    //     if (args.indexOf(element) === -1) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+    );
+   return filteredArray;
+}
 
-};
+
+console.log(removeFromArray([1,2,3,4],1,"3",4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
