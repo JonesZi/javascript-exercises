acafef3a3661d3b9c54f9f38eb70f62e8f87088e
Jonas Zi <jonas.zinnoecker@gmail.com>
2022-07-24 21:56:06 +0200

    Soleve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+const array = [];
+for (let i = 0; i < string.length; i++) {
+array[i] = string[i];
+}
+const reverseArray = array.reverse();
+const reverseString = reverseArray.join("");
+return reverseString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
