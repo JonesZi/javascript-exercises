b16a5c981aada500fcfe53ed2e0209569c51309a
Jonas Zi <jonas.zinnoecker@gmail.com>
2022-07-28 10:51:06 +0200

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(fromNumber,toNumber) {
+    if (typeof(fromNumber) !== "number" || typeof(toNumber) !== "number") {
+        return "ERROR";
+    }
+    if (fromNumber < 0 || toNumber < 0) {
+        return "ERROR";
+    }
+    let sum = 0;
+    let minNumber = Math.min(fromNumber,toNumber);
+    let maxNumber = Math.max(fromNumber,toNumber);
+    for (let i = minNumber; i <= maxNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
