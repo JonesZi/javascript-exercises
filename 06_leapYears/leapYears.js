2728030bb9464ec644099e50d173702ebc767ee2
Jonas Zi <jonas.zinnoecker@gmail.com>
2022-07-28 11:17:08 +0200

    Solve 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+// Not leap Year if:
+//     Not divisible by 4
+//     Divisible by 100, but not 400
 
+
+const leapYears = function(year) {
+    if (year % 4 !== 0) {
+        return false;
+    } else if (year % 100 === 0 && year % 400 !== 0) {
+        return false;
+    } else {
+        return true;
+    }
 };
+
+console.log(leapYears(2004));
 
 // Do not edit below this line
 module.exports = leapYears;
