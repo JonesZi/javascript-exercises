fabe048baa4c2f5f62b1ebf9c2db8e6f2aa7a6c5
Jonas Zi <jonas.zinnoecker@gmail.com>
2022-08-04 22:29:26 +0200

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function([...args]) {
+	let total = 0;
+  args.forEach(e => total += e);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function([...args]) {
+  let total = 1;
+  args.forEach(e => total *= e);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) return 1;
+  let result = 1;
+  for (let i = number; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
