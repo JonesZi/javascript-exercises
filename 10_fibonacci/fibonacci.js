a0570b318639554446dde5ae83f968c24498b23c
Jonas Zi <jonas.zinnoecker@gmail.com>
2022-08-05 22:14:31 +0200

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    number = parseInt(input);
+    if (number < 0) return "OOPS";
+    if (number === 1) return 1;
+    if (number === 2) return 1;
+    let sequence = [1,1];
+    if (number > 2) {
+        for (let i = 2; i < number; i++) {
+            sequence[i] = sequence[i-1] + sequence[i-2];
+        };
+    };
+    return sequence[number-1];
 
 };
+
+// from solutions:
+// const fibonacci = function(count) {
+//     if (count < 0) return "OOPS";
+//     if (count === 0) return 0;
+//     let a = 0;
+//     let b = 1;
+//     for (let i = 1; i < count; i++) {
+//       const temp = b;
+//       b = a + b;
+//       a = temp;
+//     }
+//     return b;
+//   };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
