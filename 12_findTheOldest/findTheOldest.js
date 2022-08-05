d81453bb5e197c046ec73f69395d9af392b16953
Jonas Zi <jonas.zinnoecker@gmail.com>
2022-08-05 23:35:12 +0200

    Adjust 12_findTheOldest to GitHub solution with reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,18 +1,35 @@
 const findTheOldest = function(array) {
-    array.forEach(element => {
-        if (!element.yearOfDeath) {
-            element.yearOfDeath = new Date().getFullYear();
-        }
+    // TWO METHODS
+    // array.forEach(element => {
+    //     if (!element.yearOfDeath) {
+    //         element.yearOfDeath = new Date().getFullYear();
+    //     }
         
-    });
-    const sorted = array.sort((a,b) => {
-        const aAge = a.yearOfDeath - a.yearOfBirth;
-        const bAge = b.yearOfDeath - b.yearOfBirth;
-        return aAge > bAge ? -1 : 1;
-    })
-    return sorted[0];
+    // });
+    // const sorted = array.sort((a,b) => {
+    //     const aAge = a.yearOfDeath - a.yearOfBirth;
+    //     const bAge = b.yearOfDeath - b.yearOfBirth;
+    //     return aAge > bAge ? -1 : 1;
+    // })
+    // return sorted[0];
 
+    //REDUCE METHOD
+    return array.reduce((oldest, person) => {
+        // if (!person.yearOfDeath) {
+        //     person.yearOfDeath = new Date().getFullYear();
+        // }
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const personAge = getAge(person.yearOfDeath,person.yearOfBirth);
+        return oldestAge < personAge ? person : oldest;
+    })
 };
+
+const getAge = function(death,birth) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
 const people = [
     {
