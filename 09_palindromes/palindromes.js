f79d391dc23c7bc90db41eaadf500e7ae8b40c2b
Jonas Zi <jonas.zinnoecker@gmail.com>
2022-08-05 21:46:26 +0200

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    cleanString = string.toLowerCase()
+                        .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g,"")
+                        .replace(/\s/g, "");
+    backwardsString = cleanString
+                        .split("")
+                        .reverse()
+                        .join("");
+    
+    if (backwardsString == cleanString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
